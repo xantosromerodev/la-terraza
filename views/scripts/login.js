14cdf155245bdb484ea0a8d167eabe9fb86b118c
Santos Romero <xantosromero@gmail.com>
2025-02-03 22:29:50 -0500

    Inicio de sesion: agregamos validaciones a los campos

--- views/scripts/login.js
+++ views/scripts/login.js
@@ -1,30 +1,54 @@
 $("#btn-ingresar").on("click", function (e) {
     e.preventDefault();
-    var data = {
-        email: $("#dni").val(),
-        password: $("#password").val()
+
+    document.getElementById('dniError').style.display = 'none';
+    document.getElementById('passwordError').style.display = 'none';
+    document.getElementById('dni').classList.remove('is-invalid');
+    document.getElementById('password').classList.remove('is-invalid');
+
+    let isValid = true
+
+    // Obtener los valores de los campos
+    const dni = document.getElementById('dni').value;
+    const password = document.getElementById('password').value;
+
+    // Validación del campo usuario (debe ser un número no vacío)
+    if (!dni || isNaN(dni)) {
+        isValid = false;
+        document.getElementById('dniError').style.display = 'block';
+        document.getElementById('dni').classList.add('is-invalid');
+      }
+
+      // Validación del campo contraseña (no vacío)
+      if (!password) {
+        isValid = false;
+        document.getElementById('passwordError').style.display = 'block';
+        document.getElementById('password').classList.add('is-invalid');
+      }
+    
+     if (isValid) {
+        var data = {
+            email: $("#dni").val(),
+            password: $("#password").val(),
+        };
+        $.post("../controller/usuario.php?op=login", data, function (e) {
+            console.log(e);
+            data = JSON.parse(e);
+            console.log(data);
+            if (data.rol == "ADMINISTRADOR") {
+                $(location).attr("href", "home.php");
+            } else if (data.rol == "AZAFATA") {
+                $(location).attr("href", "pedidos.php");
+            } else if (data.rol == "CAJA") {
+                $(location).attr("href", "caja.php");
+            }
+        });
     }
-    $.post("../controller/usuario.php?op=login", data, function (e) {
-        console.log(e);
-       data = JSON.parse(e);
-       console.log(data);
-        if (data.rol == "ADMINISTRADOR"){
-            
-            $(location).attr("href", "home.php");  
-           
-        }else if(data.rol == "AZAFATA"){
-           
-            $(location).attr("href", "pedidos.php");
-        }else if(data.rol == "CAJA"){
-           
-            $(location).attr("href", "caja.php");
-        }
-    });
 });
 
 
-// Funcionar ojito
-document.getElementById("toggle-password").addEventListener("click", function() {
+// funcion mostrar contraseña
+document.getElementById("toggle-password").addEventListener("click", function () {
     let passwordInput = document.getElementById("password");
     let icon = this.querySelector("i");
 
