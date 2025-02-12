d6423d5446eaccb75fcdc29182c999ad6a37f169
Santos Romero <xantosromero@gmail.com>
2025-02-11 21:38:27 -0500

    Panel administrador: organizamos las tarjetas

--- views/home.php
+++ views/home.php
@@ -8,50 +8,64 @@
 }
 </style>
 
-<div class="right_col" role="main">
+
+<main class="right_col" role="main">
+    <!-- tarjetas -->
     <div class="row">
-        <!-- total de pedidos -->
-        <div class="col-md-4">
-            <div class="card shadow-sm m-4">
+        <!-- pedidos completados -->
+        <div class="col-md-4 mt-2">
+            <div class="card shadow-sm">
                 <div class="card-body bg-primary text-white">
-                    <h5 class="card-title font-weight-bold">Total pedidos por mesa</h5>
-                    <h2 class="fw-bold text-right h2 font-weight-bold">350</h2>
+                    <h5 class="card-title font-weight-bold text-start">Pedidos completados</h5>
+                    <h2 class="fw-bold h2 font-weight-bold text-right">350</h2>
                     <div class="d-flex justify-content-between align-items-center">
                         <i class="fa fa-cutlery ft-icon"></i>
-                        <p class="card-text h6">+25% más que ayer</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- total recaudado -->
-        <div class="col-md-4">
-            <div class="card shadow-sm m-4">
-                <div class="card-body bg-success text-white">
-                    <h5 class="card-title font-weight-bold">Total dinero</h5>
-                    <h2 class="fw-bold text-right h2 font-weight-bold">$1,530</h2>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <i class="fa fa-money ft-icon"></i>
-                        <p class="card-text h6">+15% más que ayer</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- total de clientes -->
-        <div class="col-md-4">
-            <div class="card shadow-sm m-4">
-                <div class="card-body bg-danger text-white">
-                    <h5 class="card-title font-weight-bold">Total pedidos por delivery</h5>
-                    <h2 class="fw-bold text-right h2 font-weight-bold">50</h2>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <i class="fa fa-motorcycle ft-icon"></i>
                         <p class="card-text h6">+5% más que ayer</p>
                     </div>
                 </div>
             </div>
         </div>
+        <!-- dinero recaudado -->
+        <div class="col-md-4 mt-2">
+            <div class="card shadow-sm">
+                <div class="card-body bg-success text-white">
+                    <h5 class="card-title font-weight-bold">Dinero recaudado</h5>
+                    <h2 class="fw-bold text-right h2 font-weight-bold">$1,530</h2>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <i class="fa fa-money ft-icon"></i>
+                        <p class="card-text h6">+10% más que ayer</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- deliverys entregados -->
+        <div class="col-md-4 mt-2">
+            <div class="card shadow-sm">
+                <div class="card-body bg-danger text-white">
+                    <h5 class="card-title font-weight-bold">Delivery entregados</h5>
+                    <h2 class="fw-bold text-right h2 font-weight-bold">50</h2>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <i class="fa fa-motorcycle ft-icon"></i>
+                        <p class="card-text h6">+3% más que ayer</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
+    <div class="row">
+        <!-- grafico -->
+        <div class="col-md-6">
+
+        </div>
+        <!-- notificaciones -->
+        <div class="col-md-6">
+
+        </div>
+    </div>
+
+
+</main>
 <?php
  include '../views/footer.php';
 ?>
