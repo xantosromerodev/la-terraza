a1775bedb6b261f20df7aa48d0f29fbd6e8068c2
Santos Romero <xantosromero@gmail.com>
2025-02-13 23:08:15 -0500

    Se agrega icono perfil y notificacion

--- views/header.php
+++ views/header.php
@@ -4,15 +4,16 @@ if (strlen(session_id()) < 1)
 ?>
 <!DOCTYPE html>
 <html lang="es">
-  <head>
+
+<head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <!-- Meta, title, CSS, favicons, etc. -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" href="views/images/favicon.ico" type="image/ico" />
+    <link rel="icon" href="views/images/favicon.ico" type="image/ico" />
 
-    <title>Administracion | La Terraza</title>
+    <title>Bienvenido | La Terraza</title>
 
     <!-- Bootstrap -->
     <link href="../public/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -22,17 +23,17 @@ if (strlen(session_id()) < 1)
     <link href="../public/nprogress/nprogress.css" rel="stylesheet">
     <!-- iCheck -->
     <link href="../public/iCheck/skins/flat/green.css" rel="stylesheet">
-	
+
     <!-- bootstrap-progressbar -->
     <link href="../public/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
     <!-- JQVMap -->
-    <link href="../public/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
+    <link href="../public/jqvmap/dist/jqvmap.min.css" rel="stylesheet" />
     <!-- bootstrap-daterangepicker -->
     <link href="../public/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
     <!-- Bootstrap Select-->
     <link href="../public/bootstrap/dist/css/bootstrap-select.min.css" rel="stylesheet">
-<!-- Datatables -->
-    
+    <!-- Datatables -->
+
     <link href="../public/datatables.net-bs/css/dataTables.bootstrap.min.css" rel="stylesheet">
     <link href="../public/datatables.net-buttons-bs/css/buttons.bootstrap.min.css" rel="stylesheet">
     <link href="../public/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css" rel="stylesheet">
@@ -40,104 +41,92 @@ if (strlen(session_id()) < 1)
     <link href="../public/datatables.net-scroller-bs/css/scroller.bootstrap.min.css" rel="stylesheet">
     <!-- Custom Theme Style -->
     <link href="../public/build/css/custom.min.css" rel="stylesheet">
-  </head>
+</head>
 
-  <body class="nav-md">
+<body class="nav-md">
     <div class="container body">
-      <div class="main_container">
-        <div class="col-md-3 left_col">
-          <div class="left_col scroll-view">
-            <div class="navbar nav_title" style="border: 0;">
-              <a href="home.php" class="site_title"><i class="fa fa-hand-rock-o" aria-hidden="true"></i> <span>La Terraza</span></a>
+        <div class="main_container">
+            <div class="col-md-3 left_col">
+                <div class="left_col scroll-view">
+                    <div class="navbar nav_title">
+                        <a href="home.php" class="site_title"><i class="fa fa-hand-rock-o" aria-hidden="true"></i>
+                            <span>La Terraza</span></a>
+                    </div>
+
+                    <div class="clearfix"></div>
+
+                    <br />
+
+                    <!-- sidebar menu -->
+                    <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+                        <div class="menu_section">
+                            <!-- <h3>General</h3> -->
+                            <ul class="nav side-menu">
+                                <li><a><i class="fa fa-cog" aria-hidden="true"></i> Menú <span
+                                            class="fa fa-chevron-down"></span></a>
+                                    <ul class="nav child_menu">
+                                        <li><a href="empresa.php">Empresa</a></li>
+                                        <li><a href="perfil.php">Perfiles</a></li>
+                                        <li><a href="usuarios.php">Usuarios</a></li>
+                                    </ul>
+                                </li>
+                                <li><a><i class="fa fa-edit"></i> Inventario <span
+                                            class="fa fa-chevron-down"></span></a>
+                                    <ul class="nav child_menu">
+                                        <li><a href="form.html">Categorias</a></li>
+                                        <li><a href="form_advanced.html">Marcas</a></li>
+                                        <li><a href="form_validation.html">Productos</a></li>
+
+                                    </ul>
+                                </li>
+                                <li><a><i class="fa fa-cog"></i> Configuraciones <span
+                                            class="fa fa-chevron-down"></span></a>
+                                    <ul class="nav child_menu">
+                                        <li><a href="mesa.php">Mesas</a></li>
+                                        <li><a href="categorias.php">Categorias</a></li>
+                                        <li><a href="menu.php">Platos</a></li>
+                                        <li><a href="icons.html">Icons</a></li>
+                                        <li><a href="glyphicons.html">Glyphicons</a></li>
+                                        <li><a href="widgets.html">Widgets</a></li>
+                                        <li><a href="invoice.html">Invoice</a></li>
+                                        <li><a href="inbox.html">Inbox</a></li>
+                                        <li><a href="calendar.html">Calendar</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div>
+                    <!-- /sidebar menu -->
+                </div>
             </div>
 
-            <div class="clearfix"></div>
+            <!-- top navigation -->
+            <div class="top_nav">
+                <div class="nav_menu">
+                    <div class="nav toggle">
+                        <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+                    </div>
+                    <nav class="nav navbar-nav">
+                        <ul class=" navbar-right mr-4">
+                            <li class="nav-item dropdown open">
+                                <a href="javascript:;" class="user-profile" style="font-size: 20px;">
+                                    <i class="fa fa-bell mr-2" style="font-size: 28px;"></i>
+                                </a>
+                                <a href="javascript:;" class="user-profile" aria-haspopup="true" id="navbarDropdown"
+                                    data-toggle="dropdown" aria-expanded="false" style="font-size: 20px;">
+                                    <i class="fa fa-user" style="font-size: 28px;"></i>
+                                </a>
+                                <div class="dropdown-menu dropdown-usermenu pull-right"
+                                    aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item btn btn-default" href="#"> Perfil</a>
+                                    <a class="dropdown-item btn btn-default"
+                                        href="../controller/usuario.php?op=salir"><i
+                                            class="fa fa-sign-out pull-right"></i> Cerrar sesión</a>
+                                </div>
+                            </li>
 
-            <!-- menu profile quick info -->
-            <div class="profile clearfix">
-              <div class="profile_pic">
-                <img src="images/img.jpg" alt="..." class="img-circle profile_img">
-              </div>
-              <div class="profile_info">
-                <span>Bienvenido,</span>
-                <h2 ><?php echo $_SESSION["nombre"]?></h2>
-              </div>
+                        </ul>
+                    </nav>
+                </div>
             </div>
-            <!-- /menu profile quick info -->
-
-            <br />
-
-            <!-- sidebar menu -->
-            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
-              <div class="menu_section">
-                <!-- <h3>General</h3> -->
-                <ul class="nav side-menu">
-                  <li><a><i class="fa fa-cog" aria-hidden="true"></i> Gestión de menu <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="empresa.php">Empresa</a></li>
-                      <li><a href="perfil.php">Perfiles</a></li>
-                      <li><a href="usuarios.php">Usuarios</a></li>
-                    </ul>
-                  </li>
-                  <li><a><i class="fa fa-edit"></i> Gestión de inventario <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="form.html">Categorias</a></li>
-                      <li><a href="form_advanced.html">Marcas</a></li>
-                      <li><a href="form_validation.html">Productos</a></li>
-                      
-                    </ul>
-                  </li>
-                  <li><a><i class="fa fa-desktop"></i> Gestión de pedidos <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="mesa.php">Mesas</a></li>
-                      <li><a href="categorias.php">Categorias</a></li>
-                      <li><a href="menu.php">Platos</a></li>
-                      <li><a href="icons.html">Icons</a></li>
-                      <li><a href="glyphicons.html">Glyphicons</a></li>
-                      <li><a href="widgets.html">Widgets</a></li>
-                      <li><a href="invoice.html">Invoice</a></li>
-                      <li><a href="inbox.html">Inbox</a></li>
-                      <li><a href="calendar.html">Calendar</a></li>
-                    </ul>
-                  </li>
-                  <li><a><i class="fa fa-users"></i> Config. generales <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="tables.html">Tables</a></li>
-                      <li><a href="tables_dynamic.html">Table Dynamic</a></li>
-                    </ul>
-                  </li>
-                </ul>
-              </div>
-
-            </div>
-            <!-- /sidebar menu -->
-          </div>
-        </div>
-
-        <!-- top navigation -->
-        <div class="top_nav">
-          <div class="nav_menu">
-              <div class="nav toggle">
-                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
-              </div>
-              <nav class="nav navbar-nav">
-              <ul class=" navbar-right">
-                <li class="nav-item dropdown open" style="padding-left: 15px;">
-                  <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
-                    <img src="images/img.jpg" alt=""><?php echo $_SESSION["nombre"]?>
-                  </a>
-                  <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item"  href="javascript:;"> <?php echo "DNI: " .$_SESSION['dni']?></a>
-                      <a class="dropdown-item"  href="javascript:;">
-                        
-                        <span><?php echo "USUARIO: " .$_SESSION['email']?></span>
-                      </a>
-                  <a class="dropdown-item"  href="javascript:;"><?php echo "ROL: " .$_SESSION['rol']?></a>
-                    <a class="dropdown-item btn btn-default"  href="../controller/usuario.php?op=salir"><i class="fa fa-sign-out pull-right"></i> Cerrar Sesión</a>
-                  </div>
-                </li>
-
-              </ul>
-            </nav>
-          </div>
-        </div>
