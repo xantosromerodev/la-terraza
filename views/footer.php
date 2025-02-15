0d1c8472a9fa09f8f9bffcba5659f68f39b92579
Santos Romero <xantosromero@gmail.com>
2025-02-15 06:20:03 -0500

    footer: arreglamos el fondo del footer

--- views/footer.php
+++ views/footer.php
@@ -1,74 +1,85 @@
  <!-- footer content -->
- <footer>
-          <div class="pull-right">
-            <!-- Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a> -->
-          </div>
-          <div class="clearfix"></div>
-        </footer>
-        <!-- /footer content -->
-      </div>
-    </div>
+ <html>
 
-    <!-- jQuery -->
-    <script src="../public/jquery/dist/jquery.min.js"></script>
-    <script src="../public/jquery/dist/notify.min.js"></script>
-    <!-- Bootstrap -->
-    <script src="../public/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- FastClick -->
-    <script src="../public/fastclick/lib/fastclick.js"></script>
-    <!-- NProgress -->
-    <script src="../public/nprogress/nprogress.js"></script>
-    <!-- Chart.js -->
-    <script src="../public/Chart.js/dist/Chart.min.js"></script>
-    <!-- gauge.js -->
-    <script src="../public/gauge.js/dist/gauge.min.js"></script>
-    <!-- bootstrap-progressbar -->
-    <script src="../public/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
-    <!-- iCheck -->
-    <script src="../public/iCheck/icheck.min.js"></script>
-    <!-- Skycons -->
-    <script src="../public/skycons/skycons.js"></script>
-    <!-- Flot -->
-    <script src="../public/Flot/jquery.flot.js"></script>
-    <script src="../public/Flot/jquery.flot.pie.js"></script>
-    <script src="../public/Flot/jquery.flot.time.js"></script>
-    <script src="../public/Flot/jquery.flot.stack.js"></script>
-    <script src="../public/Flot/jquery.flot.resize.js"></script>
-    <!-- Flot plugins -->
-    <script src="../public/flot.orderbars/js/jquery.flot.orderBars.js"></script>
-    <script src="../public/flot-spline/js/jquery.flot.spline.min.js"></script>
-    <script src="../public/flot.curvedlines/curvedLines.js"></script>
-    <!-- DateJS -->
-    <script src="../public/DateJS/build/date.js"></script>
-    <!-- JQVMap -->
-    <script src="../public/jqvmap/dist/jquery.vmap.js"></script>
-    <script src="../public/jqvmap/dist/maps/jquery.vmap.world.js"></script>
-    <script src="../public/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
-    <!-- bootstrap-daterangepicker -->
-    <script src="../public/moment/min/moment.min.js"></script>
-    <script src="../public/bootstrap-daterangepicker/daterangepicker.js"></script>
+ <body>
+
+ <!-- fondo transparente footer -->
+ <style>
+    body {
+        background-color: transparent;
+    }
+ </style>
+
+
+     <footer>
+         <div class="pull-right">
+            <a href="https://sichasoft.com"></a>
+         </div>
+         <div class="clearfix"></div>
+     </footer>
+
+
+     <!-- jQuery -->
+     <script src="../public/jquery/dist/jquery.min.js"></script>
+     <script src="../public/jquery/dist/notify.min.js"></script>
+     <!-- Bootstrap -->
+     <script src="../public/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+     <!-- FastClick -->
+     <script src="../public/fastclick/lib/fastclick.js"></script>
+     <!-- NProgress -->
+     <script src="../public/nprogress/nprogress.js"></script>
+     <!-- Chart.js -->
+     <script src="../public/Chart.js/dist/Chart.min.js"></script>
+     <!-- gauge.js -->
+     <script src="../public/gauge.js/dist/gauge.min.js"></script>
+     <!-- bootstrap-progressbar -->
+     <script src="../public/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+     <!-- iCheck -->
+     <script src="../public/iCheck/icheck.min.js"></script>
+     <!-- Skycons -->
+     <script src="../public/skycons/skycons.js"></script>
+     <!-- Flot -->
+     <script src="../public/Flot/jquery.flot.js"></script>
+     <script src="../public/Flot/jquery.flot.pie.js"></script>
+     <script src="../public/Flot/jquery.flot.time.js"></script>
+     <script src="../public/Flot/jquery.flot.stack.js"></script>
+     <script src="../public/Flot/jquery.flot.resize.js"></script>
+     <!-- Flot plugins -->
+     <script src="../public/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+     <script src="../public/flot-spline/js/jquery.flot.spline.min.js"></script>
+     <script src="../public/flot.curvedlines/curvedLines.js"></script>
+     <!-- DateJS -->
+     <script src="../public/DateJS/build/date.js"></script>
+     <!-- JQVMap -->
+     <script src="../public/jqvmap/dist/jquery.vmap.js"></script>
+     <script src="../public/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+     <script src="../public/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+     <!-- bootstrap-daterangepicker -->
+     <script src="../public/moment/min/moment.min.js"></script>
+     <script src="../public/bootstrap-daterangepicker/daterangepicker.js"></script>
      <script src="../public/bootstrap/dist/js/bootstrap-select.min.js"></script>
 
-    <!-- Datatables -->
-    <script src="../public/datatables.net/js/jquery.dataTables.min.js"></script>
-    <script src="../public/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
-    <script src="../public/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-    <script src="../public/datatables.net-buttons-bs/js/buttons.bootstrap.min.js"></script>
-    <script src="../public/datatables.net-buttons/js/buttons.flash.min.js"></script>
-    <script src="../public/datatables.net-buttons/js/buttons.html5.min.js"></script>
-    <script src="../public/datatables.net-buttons/js/buttons.print.min.js"></script>
-    <script src="../public/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js"></script>
-    <script src="../public/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
-    <script src="../public/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-    <script src="../public/datatables.net-responsive-bs/js/responsive.bootstrap.js"></script>
-    <script src="../public/datatables.net-scroller/js/dataTables.scroller.min.js"></script>
-    <script src="../public/jszip/dist/jszip.min.js"></script>
-    <script src="../public/pdfmake/build/pdfmake.min.js"></script>
-    <script src="../public/pdfmake/build/vfs_fonts.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+     <!-- Datatables -->
+     <script src="../public/datatables.net/js/jquery.dataTables.min.js"></script>
+     <script src="../public/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+     <script src="../public/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+     <script src="../public/datatables.net-buttons-bs/js/buttons.bootstrap.min.js"></script>
+     <script src="../public/datatables.net-buttons/js/buttons.flash.min.js"></script>
+     <script src="../public/datatables.net-buttons/js/buttons.html5.min.js"></script>
+     <script src="../public/datatables.net-buttons/js/buttons.print.min.js"></script>
+     <script src="../public/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js"></script>
+     <script src="../public/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
+     <script src="../public/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+     <script src="../public/datatables.net-responsive-bs/js/responsive.bootstrap.js"></script>
+     <script src="../public/datatables.net-scroller/js/dataTables.scroller.min.js"></script>
+     <script src="../public/jszip/dist/jszip.min.js"></script>
+     <script src="../public/pdfmake/build/pdfmake.min.js"></script>
+     <script src="../public/pdfmake/build/vfs_fonts.js"></script>
+     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-    <!-- Custom Theme Scripts -->
-    <script src="../public/build/js/custom.min.js"></script>
-	
-  </body>
-</html>
+     <!-- Custom Theme Scripts -->
+     <script src="../public/build/js/custom.min.js"></script>
+
+ </body>
+
+ </html>
