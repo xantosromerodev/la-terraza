82276345723aab49be2b5bdae4539aff03a8b7de
Santos Romero <xantosromero@gmail.com>
2025-03-01 00:16:02 -0500

    Panel administrador: agregamos ventana modal para ver el detalle de la orden

--- views/home.php
+++ views/home.php
@@ -5,26 +5,14 @@
 <!-- libreria chart.js -->
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-<style>
-.ft-icon {
-    font-size: 25px;
-}
-
-.ft-dark-icon {
-    font-size: 20px;
-    color: #333;
-}
-</style>
-
-
 <main class="right_col" role="main">
     <!-- tarjetas -->
     <div class="row">
-        <!-- pedidos completados -->
-        <div class="col-md-4 mt-2">
+        <!-- total ventas -->
+        <section class="col-md-4 mt-2">
             <div class="card shadow-sm">
                 <div class="card-body bg-primary text-white">
-                    <h5 class="card-title font-weight-bold text-start">Pedidos completados</h5>
+                    <h5 class="card-title font-weight-bold text-start">Total ventas hoy</h5>
                     <h2 class="fw-bold h2 font-weight-bold text-right">350</h2>
                     <div class="d-flex justify-content-between align-items-center">
                         <i class="fa fa-cutlery ft-icon"></i>
@@ -32,12 +20,12 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
         <!-- dinero recaudado -->
-        <div class="col-md-4 mt-2">
+        <section class="col-md-4 mt-2">
             <div class="card shadow-sm">
                 <div class="card-body bg-success text-white">
-                    <h5 class="card-title font-weight-bold">Dinero recaudado</h5>
+                    <h5 class="card-title font-weight-bold">Total dinero hoy</h5>
                     <h2 class="fw-bold text-right h2 font-weight-bold">$1,530</h2>
                     <div class="d-flex justify-content-between align-items-center">
                         <i class="fa fa-money ft-icon"></i>
@@ -45,13 +33,13 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
 
         <!-- deliverys entregados -->
-        <div class="col-md-4 mt-2">
+        <section class="col-md-4 mt-2">
             <div class="card shadow-sm">
                 <div class="card-body bg-danger text-white">
-                    <h5 class="card-title font-weight-bold">Delivery entregados</h5>
+                    <h5 class="card-title font-weight-bold">Total delivery hoy</h5>
                     <h2 class="fw-bold text-right h2 font-weight-bold">50</h2>
                     <div class="d-flex justify-content-between align-items-center">
                         <i class="fa fa-motorcycle ft-icon"></i>
@@ -59,97 +47,149 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     </div>
     <div class="row">
         <!-- grafico -->
-        <div class="col-md-6 mt-4">
+        <section class="col-md-6 mt-4">
             <div class="card">
                 <div class="card-header">
                     <i class="fa fa-bar-chart ft-dark-icon"> </i>
-                    <span class="h6 text-body font-weight-bold ml-2">Seguimiento de pedidos</span>
+                    <span class="h6 text-body font-weight-bold ml-2">Ventas de la semana</span>
                 </div>
                 <div class="card-body">
                     <canvas id="pedidosChart"></canvas>
                 </div>
             </div>
-        </div>
-        <!-- notificaciones -->
-        <div class="col-md-6 mt-4">
+        </section>
+
+        <!-- tabla ordenes -->
+        <section class="col-md-6 mt-4">
             <div class="card">
-                <div class="card-header d-flex">
-                    <i class="fa fa-bell ft-dark-icon"></i> <span
-                        class="h6 text-body font-weight-bold ml-2">Notificaciones</span>
+                <div class="card-header">
+                    <i class="fa fa-bell ft-dark-icon"></i> <span class="h6 text-body font-weight-bold ml-2">Ordenes
+                        recientes</span>
                 </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item d-flex justify-content-between align-items-center text-body">
-                        <i class="fa fa-inbox"></i> Pedido entregado
-                        <span class="text-body">Hace 1 minuto</span>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between align-items-center text-body">
-                        <i class="fa fa-inbox"></i> Pedido entregado
-                        <span class="text-body">Hace 2 minutos</span>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between align-items-center text-body">
-                        <i class="fa fa-inbox"></i> Delivery entregado
-                        <span class="text-body">Hace 5 minutos</span>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between align-items-center text-body">
-                        <i class="fa fa-inbox"></i> Delivery entregado
-                        <span class="text-body">Hace 7 minutos</span>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between align-items-center text-body">
-                        <i class="fa fa-inbox"></i> Pedido entregado
-                        <span class="text-body">Hace 10 minutos</span>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between align-items-center text-body">
-                        <i class="fa fa-inbox"></i> Pedido entregado
-                        <span class="text-body">Hace 15 minutos</span>
-                    </li>
-                </ul>
+                <!-- tabla -->
+                <table class="table table-hover">
+                    <tbody>
+                        <tr data-toggle="modal" data-target="#modalInfo">
+                            <td><i class="fa fa-inbox"></i></td>
+                            <td>Nueva orden solicitada</td>
+                            <td>Hace un instante</td>
+                        </tr>
+                        <tr data-toggle="modal" data-target="#modalInfo">
+                            <td><i class="fa fa-inbox"></i></td>
+                            <td>Nueva orden solicitada</td>
+                            <td>Hace 02 minutos</td>
+                        </tr>
+                        <tr data-toggle="modal" data-target="#modalInfo">
+                            <td><i class="fa fa-inbox"></i></td>
+                            <td>Nueva orden solicitada</td>
+                            <td>Hace 03 minutos</td>
+                        </tr>
+                        <tr data-toggle="modal" data-target="#modalInfo">
+                            <td><i class="fa fa-inbox"></i></td>
+                            <td>Nueva orden solicitada</td>
+                            <td>Hace 04 minutos</td>
+                        </tr>
+                        <tr data-toggle="modal" data-target="#modalInfo">
+                            <td><i class="fa fa-inbox"></i></td>
+                            <td>Nueva orden solicitada</td>
+                            <td>Hace 05 minutos</td>
+                        </tr>
+                        <tr data-toggle="modal" data-target="#modalInfo">
+                            <td><i class="fa fa-inbox"></i></td>
+                            <td>Nueva orden solicitada</td>
+                            <td>Hace 06 minutos</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </section>
+
+        <!-- detalle ordenes -->
+        <div class="modal" id="modalInfo">
+            <div class="modal-dialog modal-lg modal-dialog-centered">
+                <div class="modal-content">
+
+                    <!-- Modal Header -->
+                    <div class="modal-header">
+                        <h5 class="modal-title">Detalle de la orden</h5>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+
+                    <!-- Modal body -->
+                    <div class="modal-body">
+                        <table class="table table-bordered">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th>Comanda</th>
+                                    <th>Producto</th>
+                                    <th>Cantidad</th>
+                                    <th>Detalles</th>
+                                    <th>Estado</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>P001</td>
+                                    <td>Yuquitas fritas</td>
+                                    <td>2</td>
+                                    <td>Bien fritas, con sal y para llevar.</td>
+                                    <td><span class="badge badge-danger">Pendiente</span></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <!-- Modal footer -->
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
 
-    <script>
-    // Datos de pedidos
-    var pedidosData = {
-        labels: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'],
-        datasets: [{
-            label: 'Total ($)',
-            data: [150, 200, 180, 220, 250, 300, 190], // Ventas de cada día
-            backgroundColor: 'rgba(0, 123, 255, 0.8)', // Color de las barras
-            borderColor: 'rgba(0, 123, 255, 1)', // Color del borde
-            borderWidth: 1
-        }]
-    };
+        <script>
+        // Datos de pedidos
+        var pedidosData = {
+            labels: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'],
+            datasets: [{
+                label: 'Total de ventas ($)',
+                data: [150, 200, 180, 220, 250, 300, 190], // Ventas de cada día
+                backgroundColor: 'rgba(0, 123, 255, 0.8)', // Color de las barras
+                borderColor: 'rgba(0, 123, 255, 1)', // Color del borde
+                borderWidth: 1
+            }]
+        };
 
-    // Opciones del gráfico
-    var pedidosOptions = {
-        responsive: true,
-        scales: {
-            y: {
-                beginAtZero: true,
-                ticks: {
-                    stepSize: 50
+        // Opciones del gráfico
+        var pedidosOptions = {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        stepSize: 50
+                    }
                 }
             }
-        }
-    };
+        };
 
-    // Crear gráfico
-    var ctx = document.getElementById('pedidosChart').getContext('2d');
-    var pedidosChart = new Chart(ctx, {
-        type: 'bar', // Tipo de gráfico (barras)
-        data: pedidosData,
-        options: pedidosOptions
-    });
-    </script>
+        // Crear gráfico
+        var ctx = document.getElementById('pedidosChart').getContext('2d');
+        var pedidosChart = new Chart(ctx, {
+            type: 'bar', // Tipo de gráfico (barras)
+            data: pedidosData,
+            options: pedidosOptions
+        });
+        </script>
 
 
-    <!-- Bootstrap JS y dependencias -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+        <!-- Bootstrap JS y dependencias -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
 
 </main>
 <?php
